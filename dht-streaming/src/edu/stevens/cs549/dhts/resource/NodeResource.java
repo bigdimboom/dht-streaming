6c1e720003f40cc8146f72d0a78f5c87a1f8955c
lluo1 <lluo1@stevens.edu>
2015-07-23 15:07:32 -0400

    Initial Cmiit

---
+++ dht-streaming/src/edu/stevens/cs549/dhts/resource/NodeResource.java
@@ -0,0 +1,118 @@
+package edu.stevens.cs549.dhts.resource;
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.glassfish.jersey.media.sse.SseFeature;
+
+@Path("/dht")
+public class NodeResource {
+
+	/*
+	 * Web service API.
+	 * 
+	 * TODO: Fill in the missing operations.
+	 */
+
+	Logger log = Logger.getLogger(NodeResource.class.getCanonicalName());
+
+	@Context
+	UriInfo uriInfo;
+
+	@Context
+	HttpHeaders headers;
+
+	@GET
+	@Path("info")
+	@Produces("application/xml")
+	public Response getNodeInfoXML() {
+		return new NodeService(headers, uriInfo).getNodeInfo();
+	}
+
+	@GET
+	@Path("info")
+	@Produces("application/json")
+	public Response getNodeInfoJSON() {
+		return new NodeService(headers, uriInfo).getNodeInfo();
+	}
+
+	@GET
+	@Path("pred")
+	@Produces("application/xml")
+	public Response getPred() {
+		return new NodeService(headers, uriInfo).getPred();
+	}
+
+	@GET
+	@Path("succ")
+	@Produces("application/xml")
+	public Response getSucc() {
+		return new NodeService(headers, uriInfo).getSucc();
+	}
+
+	@GET
+	@Path("find")
+	@Produces("application/xml")
+	public Response findSuccessor(@QueryParam("id") String index) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).findSuccessor(id);
+	}
+
+	@PUT
+	@Path("notify")
+	@Consumes("application/xml")
+	@Produces("application/xml")
+	/*
+	 * Actually returns a TableRep (annotated with @XmlRootElement)
+	 */
+	public Response putNotify(TableRep predDb) {
+		/*
+		 * See the comment for WebClient::notify (the client side of this
+		 * logic).
+		 */
+		return new NodeService(headers, uriInfo).notify(predDb);
+		// NodeInfo p = predDb.getInfo();
+	}
+
+	@GET
+	@Path("finger")
+	@Produces("application/xml")
+	public Response closestPrecedingFinger(@QueryParam("id") String index) {
+		int id = Integer.parseInt(index);
+		return new NodeService(headers, uriInfo).closestPrecedingFinger(id);
+	}
+
+	@GET
+	@Produces("application/xml")
+	public Response getKey(@QueryParam("key") String key) {
+		return new NodeService(headers, uriInfo).getKey(key);
+	}
+
+	@PUT
+	public Response add(@QueryParam("key") String k, @QueryParam("val") String v) {
+		return new NodeService(headers, uriInfo).add(k, v);
+	}
+
+	@DELETE
+	public Response delete(@QueryParam("key") String k, @QueryParam("val") String v) {
+		return new NodeService(headers, uriInfo).delete(k, v);
+	}
+
+	@DELETE
+	@Path("listen")
+	public void listenOff(@QueryParam("id") int id, @QueryParam("key") String key) {
+		new NodeService(headers, uriInfo).stopListening(id, key);
+	}
+
+}
